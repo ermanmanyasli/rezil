85c3fb948d49d09bad00479c9797237d627b1f55
Erman Manyasli <ermanmanyasli@gmail.com>
2026-09-04 11:15:27 +0300

    Initial Commit

---
+++ rezil/rezilApp.swift
@@ -0,0 +1,17 @@
+//
+//  rezilApp.swift
+//  rezil
+//
+//  Created by Erman Manyasli on 4.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct rezilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ rezilTests/rezilTests.swift
@@ -0,0 +1,19 @@
+//
+//  rezilTests.swift
+//  rezilTests
+//
+//  Created by Erman Manyasli on 4.09.2026.
+//
+
+import Testing
+@testable import rezil
+
+struct rezilTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
